a984c8538a64bc003402d88b217c6ff26fe830f4
Axentra-Nexus <Axentra-Nexus@users.noreply.github.com>
2026-04-27 13:39:48 +0000

    deploy: bd9f36a7705738700e404d0bce679910071d0c08

--- server/assets/sales-growth-Cyf-Nmdc.js
+++
@@ -1,8 +0,0 @@
-const lendingFinance = "/axentra-nexus/assets/lending-finance-DF6dEnSZ.jpg";
-const cxAgent = "/axentra-nexus/assets/cx-agent-CSJpQeWB.jpg";
-const salesGrowth = "/axentra-nexus/assets/sales-growth-DSU8RxRC.jpg";
-export {
-  cxAgent as c,
-  lendingFinance as l,
-  salesGrowth as s
-};
